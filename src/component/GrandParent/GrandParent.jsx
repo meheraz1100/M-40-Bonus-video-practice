57cd5c3e70506c470418c036240822b895eb7a40
meheraz1100 <meheraz1100@gmail.com>
2024-04-02 05:17:15 +0600

    Grand parent component added

--- src/component/GrandParent/GrandParent.jsx
+++ src/component/GrandParent/GrandParent.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
+import Parent from "../Parent/Parent";
 
 const GrandParent = () => {
+    const [property, setProperty] = useState(100)
     return (
         <div>
             <h1>Grand Parent</h1>
-
+            <Parent property={property}></Parent>
         </div>
     );
 };
