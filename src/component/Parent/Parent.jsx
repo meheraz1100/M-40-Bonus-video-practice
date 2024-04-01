9f54c4bd673191ab4e0b3d8d236a25e063761804
meheraz1100 <meheraz1100@gmail.com>
2024-04-02 05:17:26 +0600

    parent component added

--- src/component/Parent/Parent.jsx
+++ src/component/Parent/Parent.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Child from '../Child/Child';
+import './Parent.css'
 
-const Parent = () => {
+const Parent = ({property}) => {
     return (
         <div>
             <h1>Parent</h1>
+            <Child property={property}></Child>
         </div>
     );
 };
